488d408f148e01fcbc4d1791c2e640674961d2f8
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-11-14 20:00:37 +0600

    sav

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,68 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Добро пожаловать в простую игру!");
+        System.out.println("Добро пожаловать в игру !");
+        System.out.println("№ 1: Найди число ( FindNumber )");
+        System.out.println("№ 2: Угадаю день рождение ( FindBirthday )");
 
-        int randomNumber = (int) (Math.random() * 50) + 1;
-        boolean hasGuessed = false;
+        System.out.print("Напишите одно из них: ");
+        boolean hasSelectedGame = false;
+        Scanner Selected = new Scanner(System.in);
 
-        System.out.println("Я загадал число от 1 до 50. Попробуйте угадать!");
-
-        while (!hasGuessed) {
-            System.out.print("Введите ваше предположение: ");
-            int guess = scanner.nextInt();
-
-            if (guess == randomNumber) {
-                hasGuessed = true;
-                System.out.println("Поздравляю! Вы угадали число!");
-            } else if (guess < randomNumber) {
-                System.out.println("Загаданное число больше.");
+        while (!hasSelectedGame) {
+            String UserSelected = Selected.next();
+            if (UserSelected.equals("FindNumber")) {
+                gameOne();
+                hasSelectedGame = true;
+            } else if (UserSelected.equals("FindBirthday")) {
+                gameTwo();
+                hasSelectedGame = true;
             } else {
-                System.out.println("Загаданное число меньше.");
+                System.out.print("Напишите правильно");
             }
         }
-
-        scanner.close();
     }
+        public static void gameOne() {
+            Scanner input = new Scanner(System.in);
+            int randomNumber = (int) (Math.random() * 50) + 1;
+            boolean hasGuessed = false;
+            System.out.println("Я загадал число от 1 до 50. Попробуйте угадать!");
+            while (!hasGuessed) {
+                System.out.print("Введите ваше предположение: ");
+                int guess = input.nextInt();
+                if (guess == randomNumber) {
+                    hasGuessed = true;
+                    System.out.println("Поздравляю! Вы угадали число!");
+                } else if (guess < randomNumber) {
+                    System.out.println("Загаданное число больше.");
+                } else {
+                    System.out.println("Загаданное число меньше.");
+                }
+            }
+            input.close();
+        }
+        public static void gameTwo(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Привет! Ответь на несколько вопросов, чтобы я угадал твой день рождения.");
+            System.out.println("Ответь 'да' или 'нет'.");
+            int day = 0;
+            System.out.println("Это число от 1 до 10?");
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("да")) {
+                day += 1;
+            }
+            System.out.println("Это число от 11 до 20?");
+            answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("да")) {
+                day += 10;
+            }
+            System.out.println("Это число от 21 до 31?");
+            answer = scanner.nextLine();
+
+            if (answer.equalsIgnoreCase("да")) {
+                day += 20;
+            }
+            System.out.println("Твой день рождения " + day + " число!");
+            scanner.close();
+        }
 }
